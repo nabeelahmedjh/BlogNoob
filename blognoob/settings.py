d5118cbad6bf726496eb581ea28ae2d02210578a
nabeelahmedjh <abdulnabeel40@gmail.com>
2022-09-01 16:23:23 +0500

    updated static files

--- blognoob/settings.py
+++ blognoob/settings.py
@@ -50,6 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'blognoob.urls'
@@ -121,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS =[
     BASE_DIR / 'static/',
